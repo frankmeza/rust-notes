e8e4b3fb1efb471797a6fbb33c62e3be3d06ac51
Frank Meza <frankmezapdx@gmail.com>
2019-03-13 22:25:27 -0700

    move much code to lib.rs, finish chapter 12-3

--- the_rust_book/chapter_12_io_cli/src/main.rs
+++ the_rust_book/chapter_12_io_cli/src/main.rs
@@ -1,40 +1,8 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            // this is the &'static str in returned Result
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-// aside: () is the unit type
-// Box<dyn Error> returns a type that impl Error trait,
-// to allow all kinds of Error. All kinds.
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // ? returns the value from the current fn to caller.
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-    // This Ok(()) syntax might look a bit strange at first, but using ()
-    // like this is the idiomatic way to indicate that we’re calling run
-    // for its side effects only; it doesn’t return a value we need.
-    Ok(())
-}
+use chapter_12_io_cli;
+use chapter_12_io_cli::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -52,7 +20,7 @@ fn main() {
 
     // if-let is used instead of unwrap_or_else() for error checking
     // because the fn would only return the unit type () .
-    if let Err(err) = run(config) {
+    if let Err(err) = chapter_12_io_cli::run(config) {
         println!("Application Error: {}", err);
 
         process::exit(1);
