1cb09bc754d0ea5eb5706d6c2b6fe67b8215549e
Frank Meza <frankmezapdx@gmail.com>
2019-03-13 21:18:35 -0700

    add comments

--- the_rust_book/chapter_12_io_cli/src/main.rs
+++ the_rust_book/chapter_12_io_cli/src/main.rs
@@ -10,6 +10,7 @@ struct Config {
 impl Config {
     fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
+            // this is the &'static str in returned Result
             return Err("not enough arguments");
         }
 
@@ -23,7 +24,10 @@ impl Config {
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    // unwrap_or_else is used for error handling
     let config = Config::new(&args).unwrap_or_else(|err| {
+        // this is much better error messaging for users
+        // than the compiler's stock developer debug messages
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
