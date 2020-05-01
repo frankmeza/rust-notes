61a7ed84b4205824f4e2c0022cbaa46e3a81df57
Frank Meza <frankmezapdx@gmail.com>
2020-04-30 17:22:26 -0700

    add get request check

--- 20_tcp_server/src/main.rs
+++ 20_tcp_server/src/main.rs
@@ -1,6 +1,7 @@
+use std::fs;
 use std::io::prelude::*;
-use std::net::TcpStream;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -13,8 +14,25 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
+    // create an empty buffer
     let mut buffer = [0; 512];
+    // read the stream data into the buffer
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let get_request_prefix = b"GET / HTTP/1.1\r\n";
+
+    if buffer.starts_with(get_request_prefix) {
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!("{}{}", status_line, contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
